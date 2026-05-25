2de299172342f6020be53bba687d55637f8dc466
🩶─⃝‌‌Sɪʏꫝᴍ <makimabot8@gmail.com>
2026-05-25 10:57:45 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -537,7 +537,7 @@ async function createWelcomeCard({
     pGrad.addColorStop(1, 'rgba(46,204,113,1)');
     ctx.fillStyle = pGrad;
     ctx.shadowColor = 'rgba(100,200,255,0.55)'; ctx.shadowBlur = 16;
-    ctx.fillText('Powered By Xiyam69x, cx, cy + 8);
+    ctx.fillText('Powered By Xiyam69x', cx, cy + 8);
     ctx.restore();
   }
 
